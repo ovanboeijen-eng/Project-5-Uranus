443d4ce541ae6ed55a3ef38c7f8ca75fb59de1da
Odette <Gebruiker@DESKTOP-6REEEKV>
2026-01-14 09:57:54 +0100

    geupdate

--- Project #5/Pages/DataBase/DataBase.cs
+++ Project #5/Pages/DataBase/DataBase.cs
@@ -5,49 +5,32 @@ namespace Project__5.Pages.DataBase
 {
     public class DataBase
     {
+        private MySqlConnectionStringBuilder builder = new ();
         private MySqlConnection conn = new MySqlConnection("Server=192.168.42.3;Database=CampKeeper;User Id=CampKeeper;Password=@@rdaPPel23");
         public MySqlConnection GetConnection()
         {
+            builder.Server = "";
+            builder.Database = "";
+            builder.UserID = "";
+            builder.Password = "";
+            conn = new MySqlConnection(builder.ConnectionString);
+
             return conn; 
         }
 
 
-        public void GetHuurder()
-        {
-            conn.Open();
-            string query = "SELECT * FROM huurders";
-            using (MySqlCommand cmd = new MySqlCommand(query, conn))
-            {
-                using (MySqlDataReader reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        string naam = reader["email"].ToString();
-                        string email = reader["wachtwoord"].ToString();
-                    }
-                    conn.Close();
-
-                }
-            }
-        }
-
         public bool GetUserByEmailAndPassword(string email, string password)
         {
             conn.Open();
-            string query = "SELECT * FROM gebruikers WHERE email = @Email AND wachtwoord = @Wachtwoord";
+            string query = "SELECT COUNT(*) FROM Huurder WHERE email = @Email AND wachtwoord = @Wachtwoord";
 
             using (MySqlCommand cmd = new MySqlCommand(query, conn))
             {
                 cmd.Parameters.AddWithValue("@Email", email);
                 cmd.Parameters.AddWithValue("@Wachtwoord", password);
 
-                using (MySqlDataReader reader = cmd.ExecuteReader())
-                {
-
-                    bool userExists = reader.HasRows;
-
-                    conn.Close();
-                    return userExists;
+                var count = cmd.ExecuteScalar();
+                return (count != null && Convert.ToInt32(count) > 0)
                 }
             }
         }
